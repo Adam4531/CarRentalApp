2b0b98611f663c85e84b7d95f19c9bfa9507ade7
igorkawczynski54 <igor.kawczynski@zetosoftware.pl>
2022-07-05 12:14:24 +0200

    Creating project

--- src/main/java/pl/zetosoftware/ZetosoftwareApplication.java
+++ src/main/java/pl/zetosoftware/ZetosoftwareApplication.java
@@ -3,6 +3,7 @@ package pl.zetosoftware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ZetosoftwareApplication {
 
