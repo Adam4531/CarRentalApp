97aab97e9c94c18a8e7d4d2b80f3d2f8abf6325b
igorkawczynski54 <igor.kawczynski@zetosoftware.pl>
2022-07-05 17:04:59 +0200

    Update gitignore #2

--- src/main/java/pl/zetosoftware/ZetosoftwareApplication.java
+++ src/main/java/pl/zetosoftware/ZetosoftwareApplication.java
@@ -8,12 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 public class ZetosoftwareApplication {
-	//Random comment
 
-	public static void main(String[] args) {
+
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ZetosoftwareApplication.class, args);
 	}
-//komentarz bylejaki
 }
-//komentarz igora
-//komentarz
--- src/main/java/pl/zetosoftware/user/User.java
+++ src/main/java/pl/zetosoftware/user/User.java
@@ -1,24 +1,4 @@
 package pl.zetosoftware.user;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-
-@Getter
-@NoArgsConstructor
-@Entity
-@Table(name = "users")
 public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String firstName;
-
-    public User(Long id, String firstName) {
-        this.id = id;
-        this.firstName = firstName;
-    }
-
 }
