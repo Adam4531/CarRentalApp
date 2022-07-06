c68530d07c3fdc2434b463e94e0f9b6c63f2d82b
AdamRoz <adam.rozwadowski@zetosoftware.pl>
2022-07-06 15:44:28 +0200

    data injection for users table

---
+++ src/main/resources/db.migration/V4__InsertData.sql
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+insert into users (ID, login, password, first_name, second_name, phone_number, email, pesel) values
+(1, 'ngartin0', 'Qu217b7', 'Nara', 'Gartin', '609724448', 'ngartin0@lulu.com', 87092048473),
+(2, 'ecureton1', 'iWGFaVu5L3oZ', 'Eustace', 'Cureton', '716790081', 'ecureton1@4shared.com', 87080246827),
+(3, 'nleggett2', 'gygYsiCIYzi', 'Nelly', 'Leggett', '390985484', 'nleggett2@cdc.gov', 75052895481),
+(4, 'rchalcot3', '6kazAY', 'Rosy', 'Chalcot', '339948779', 'rchalcot3@amazon.co.uk', 73022866264),
+(5, 'cruffey4', 'h95cB1', 'Carole', 'Ruffey', '553897019', 'cruffey4@kickstarter.com', 90050444581),
+(6, 'pocullen5', 'GILeL2vauS', 'Pavia', 'O''Cullen', '716989136', 'pocullen5@businessinsider.com', 75031665869),
+(7, 'jlocke6', 'g8D7uU', 'Jobi', 'Locke', '821585705', 'jlocke6@geocities.com', 86020244198),
+(8, 'gstower7', 'azeZvOJMiKtw', 'Goober', 'Stower', '829966115', 'gstower7@plala.or.jp', 99042771212),
+(9, 'efarquar8', 'peerhydyALcq', 'Everett', 'Farquar', '311088735', 'efarquar8@e-recht24.de', 83091536322),
+(10, 'gbelding9', 'IbjGDrti', 'Georges', 'Belding', '210836552', 'gbelding9@livejournal.com', 67091581186),
+(11, 'debhardta', 'k3xeOAG', 'Davide', 'Ebhardt', '822345502', 'debhardta@about.com', 70082925836),
+(12, 'dguillotinb', 'CGCa4E2lG', 'Denver', 'Guillotin', '588052271', 'dguillotinb@dailymail.co.uk', 73012169458),
+(13, 'emutterc', 'ROWsaKK', 'Erastus', 'Mutter', '643233175', 'emutterc@cargocollective.com', 78052322155),
+(14, 'ktrunkfieldd', 'oe8ejq7oo', 'Kele', 'Trunkfield', '922928935', 'ktrunkfieldd@google.co.uk', 95010641736),
+(15, 'datleye', '9PCN7D75bVSb', 'Doug', 'Atley', '517857919', 'datleye@technorati.com', 64082445317),
+(16, 'sbraunerf', 'unH0rsjBkt', 'Salim', 'Brauner', '812775564', 'sbraunerf@ucoz.com', 62011838742),
+(17, 'rpierrepointg', 'ZLsKDsNlm', 'Roxi', 'Pierrepoint', '718074094', 'rpierrepointg@miibeian.gov.cn', 83032119331),
+(18, 'khanwellh', 'w14ksTiBf22e', 'Konstantine', 'Hanwell', '933165805', 'khanwellh@fotki.com', 67040981166),
+(19, 'dtoureti', 'lgdy6rU74Zv', 'Darcy', 'Touret', '483549697', 'dtoureti@merriam-webster.com', 99080175524),
+(20, 'ebruckentalj', 'oSTRP1ZMvMgt', 'Erie', 'Bruckental', '773864681', 'ebruckentalj@gnu.org', 88072037757);
+
