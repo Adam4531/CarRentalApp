06cb0fb20ecbe0156b23563db693570019fc41be
martyna4242 <martyna.sip@zetosoftware.pl>
2022-07-06 13:58:12 +0200

    created data tables

--- src/main/resources/db.migration/V1__InitTables.sql
+++ src/main/resources/db.migration/V1__InitTables.sql
@@ -10,11 +10,36 @@ CREATE TABLE IF NOT EXISTS users(
     CONSTRAINT users_id_pkey PRIMARY KEY(ID)
     )
 
--- CREATE TABLE IF NOT EXISTS reservations(
---     ID serial NOT NULL,
---
---     )
---
--- CREATE TABLE cars(ID serial NOT NULL)
+CREATE TYPE cars_body_type AS ENUM('Coupe', 'Sedan', 'SUV', 'Kombi', 'Hatchback', 'Kabriolet');
+CREATE TYPE cars_type_of_fuel AS ENUM('Benzyna', 'Diesel', 'Electric', 'LPG');
+CREATE TABLE IF NOT EXISTS cars(
+    ID serial NOT NULL,
+    brand VARCHAR(20),
+    model VARCHAR(20),
+    engine_capacity numeric(2,1),
+    body_type cars_body_type NOT NULL,
+    type_of_fuel cars_type_of_fuel NOT NULL,
+    new_car_cost numeric(6,2),
+    production_year int NOT NULL,
+    CONSTRAINT cars_id_pkey PRIMARY KEY(ID)
+    )
+
+CREATE TYPE reservation_status AS ENUM('Free', 'Reserved', 'Taken');
+
+CREATE TABLE IF NOT EXISTS reservations(
+    ID serial NOT NULL,
+    user_id int NOT NULL,
+    car_id int NOT NULL,
+    date_start date NOT NULL,
+    date_end date NOT NULL,
+    cost numeric(6,2) NOT NULL,
+    payment_in_advance numeric(5,2) NOT NULL,
+    status reservation_status NOT NULL,
+    CONSTRAINT reservations_id_pkey PRIMARY KEY(ID),
+    CONSTRAINT user_id_pkey FOREIGN KEY(user_id) REFERENCES users(ID),
+    CONSTRAINT car_id_pkey FOREIGN KEY(car_id) REFERENCES cars(ID)
+    )
+
+
 
 
