a3608953980e87adc2c600fa1804c2df9e7f3251
AdamRoz <adam.rozwadowski@zetosoftware.pl>
2022-07-07 11:40:14 +0200

    Reservation controller almost done

--- src/main/java/pl/zetosoftware/reservation/ReservationController.java
+++ src/main/java/pl/zetosoftware/reservation/ReservationController.java
@@ -1,4 +1,11 @@
 package pl.zetosoftware.reservation;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cars")
 public class ReservationController {
+
+    private final ReservationService reservationService;
 }
