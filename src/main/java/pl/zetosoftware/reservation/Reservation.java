72db59d7d38823552cf11cb7422cf9aedafe118c
AdamRoz <adam.rozwadowski@zetosoftware.pl>
2022-07-07 11:23:24 +0200

    annotation to reservation class added

--- src/main/java/pl/zetosoftware/reservation/Reservation.java
+++ src/main/java/pl/zetosoftware/reservation/Reservation.java
@@ -1,4 +1,13 @@
 package pl.zetosoftware.reservation;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class Reservation {
+
 }
