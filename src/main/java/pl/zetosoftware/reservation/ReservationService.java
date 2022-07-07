b451bc3ebbc2575ab4221f682bde1e6436ba3281
martyna4242 <martyna.sip@zetosoftware.pl>
2022-07-07 12:45:09 +0200

    reservation service done

--- src/main/java/pl/zetosoftware/reservation/ReservationService.java
+++ src/main/java/pl/zetosoftware/reservation/ReservationService.java
@@ -1,4 +1,21 @@
 package pl.zetosoftware.reservation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+
+    @Autowired
+    public ReservationService(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+
+    }
+    public Reservation createReservation(Reservation reservation){
+        return reservationRepository.save(reservation);
+
+
+    }
 }
