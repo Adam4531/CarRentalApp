d34c5bfcbcc024b545d3fa5111c7ab6232d8b3dc
AdamRoz <adam.rozwadowski@zetosoftware.pl>
2022-07-08 17:13:56 +0200

    Changes in Car enums

--- src/main/java/pl/zetosoftware/car/enums/BodyType.java
+++ src/main/java/pl/zetosoftware/car/enums/BodyType.java
@@ -1,16 +1,10 @@
 package pl.zetosoftware.car.enums;
 
 public enum BodyType {
-    Coupe("Coupe"),
-    Sedan("Sedan"),
-    SUV("SUV"),
-    Station_Wagon("Station_Wagon"),
-    Hatchback("Hatchback"),
-    Cabriolet("Cabriolet");
-
-    private String name;
-
-    BodyType(String name) {
-        this.name = name;
-    }
+    COUPE,
+    SEDAN,
+    SUV,
+    STATION_WAGON,
+    HATCHBACK,
+    CABRIOLET
 }
--- src/main/java/pl/zetosoftware/car/enums/TypeOfFuel.java
+++ src/main/java/pl/zetosoftware/car/enums/TypeOfFuel.java
@@ -1,14 +1,8 @@
 package pl.zetosoftware.car.enums;
 
 public enum TypeOfFuel {
-    BENZINE("Benzine"),
-    Diesel("Diesel"),
-    Electric("Electric"),
-    LPG("LPG");
-
-    private String name;
-
-    TypeOfFuel(String name) {
-        this.name = name;
-    }
+    BENZINE,
+    DIESEL,
+    ELECTRIC,
+    LPG
 }
