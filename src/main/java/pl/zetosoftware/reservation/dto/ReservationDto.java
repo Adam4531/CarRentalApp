90277ce66bbd9e1159f21ca2537539ef99e2b642
krychuzeto <grzegorz.krych@zetosoftware.pl>
2022-07-11 12:04:21 +0200

    Removed unused imports

--- src/main/java/pl/zetosoftware/reservation/dto/ReservationDto.java
+++ src/main/java/pl/zetosoftware/reservation/dto/ReservationDto.java
@@ -1,15 +1,12 @@
 package pl.zetosoftware.reservation.dto;
 
 import lombok.Builder;
-import pl.zetosoftware.annotations.GenerateTypescript;
 import pl.zetosoftware.reservation.enums.Status;
-import pl.zetosoftware.reservation.value_objects.Cost;
-import pl.zetosoftware.reservation.value_objects.PaymentInAdvance;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@GenerateTypescript
+
 public record ReservationDto(Long userId, Long carId, LocalDateTime dateStart, LocalDateTime dateEnd, BigDecimal cost, BigDecimal paymentInAdvance, Status status) {
 
     @Builder
