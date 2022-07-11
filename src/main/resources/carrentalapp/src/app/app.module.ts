ba3b87e792c50bb5333f615b1628fe39c49b7810
igorkawczynski54 <igor.kawczynski@zetosoftware.pl>
2022-07-11 14:21:10 +0200

    Modified app.module.ts, changed imports...

--- src/main/resources/carrentalapp/src/app/app.module.ts
+++ src/main/resources/carrentalapp/src/app/app.module.ts
@@ -1,28 +1,15 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { CarService } from './car.service';
-import { ReservationsComponent } from './reservations/reservations.component';
-=======
-import { CarService } from './car/car.service';
 import {ButtonModule} from 'primeng/button';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {InputTextModule} from 'primeng/inputtext';
-import { LoginComponent } from './login/login/login.component';
 import {TableModule} from 'primeng/table';
->>>>>>> d68663dbdc259142351fc67b3ba98d3aa1e0339d
 
 @NgModule({
   declarations: [
-    AppComponent,
-<<<<<<< HEAD
-    ReservationsComponent
-=======
-    LoginComponent
->>>>>>> d68663dbdc259142351fc67b3ba98d3aa1e0339d
+    AppComponent
   ],
   imports: [
     BrowserModule,
