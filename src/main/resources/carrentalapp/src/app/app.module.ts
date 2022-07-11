61f7044e5fc3b9162af858b8c1543fad872b75f9
igorkawczynski54 <igor.kawczynski@zetosoftware.pl>
2022-07-11 13:17:59 +0200

    Changed User,Car,Reservation class names to UserEntity,CarEntity,ReservationEntity + imported PrimeNG...

--- src/main/resources/carrentalapp/src/app/app.module.ts
+++ src/main/resources/carrentalapp/src/app/app.module.ts
@@ -3,17 +3,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CarService } from './car.service';
+import { CarService } from './car/car.service';
+import {ButtonModule} from 'primeng/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {InputTextModule} from 'primeng/inputtext';
+import { LoginComponent } from './login/login/login.component';
+import {TableModule} from 'primeng/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    TableModule
   ],
-  providers: [], // z nawiasu mozliwe ze do wywalenia
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
