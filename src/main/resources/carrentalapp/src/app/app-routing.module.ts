55893545ab5bb26c309312bbcfc1ed2d80ed7c45
adam.trentowski <adam.trentowski@zetosoftware.pl>
2022-07-12 18:19:07 +0200

    Adding prototype registration form

--- src/main/resources/carrentalapp/src/app/app-routing.module.ts
+++ src/main/resources/carrentalapp/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { CarComponent } from './car/car.component';
 import { ReservationsComponent } from './reservations/reservations.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   { path: 'cars', component: CarComponent },
   { path: 'reservations', component: ReservationsComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'users', component: UserComponent}
+  { path: 'users', component: UserComponent},
+  { path: 'registration', component: RegistrationComponent}
 ];
 
 @NgModule({
