b244f4c5d4eaba3885df5166aef315f134c8b757
adam.trentowski <adam.trentowski@zetosoftware.pl>
2022-07-15 14:55:07 +0200

    Front: added registration page

--- src/main/resources/carrentalapp/src/app/registration/registration.component.ts
+++ src/main/resources/carrentalapp/src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -8,17 +9,21 @@ import { Component, OnInit } from '@angular/core';
 
 export class RegistrationComponent implements OnInit {
 
-  letterNumberDotDashUnderscore: RegExp = /[a-zA-Z\d._-]+$/
+  firstName!: string;
+  lastName!: string;
+  username!: string;
+  password1!: string;
+  password2!: string;
+  email!: string;
+  phoneNumber!: string;
+  pesel!: string;
 
-  emailPattern: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  constructor(private router: Router){
+  }
 
-  password1: string = "";
-  password2: string = "";
-  phoneNumber: string ="";
-  pesel: string="";
-
-  // public btnClick(url: string): void {
-  //   };
+  public btnClick(url: string): void {
+    this.router.navigateByUrl(url);
+    };
 
   ngOnInit(): void {
   }
